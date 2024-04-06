ea7a080c6bc61e5fcd3fd61fa0c02c456f4b00da
Takatoshi <nabe1020@gmail.com>
2024-04-06 15:34:23 +0900

    o

--- kreg.js
+++ kreg.js
@@ -1,16 +1,29 @@
-function makeRegisterJson(name, grade) {
-        const json = {
-            message: 'register',
-            params: {
-                name: name,
-                grade: grade
-            }
-        };
-        return json;
+const endPoint = 'https://script.google.com/macros/s/AKfycbyxVh84MzVv0ng2VfSlWfjWi8qc8bI2ROFYSQfBjCJMGXYDFWeT7NU0G0d0BI6GcRNrRg/exec';
+
+async function post(message, param) {
+    const data = {
+        message: message,
+        param: param
+    };
+    const response = await fetch(endPoint, {
+        method: 'POST',
+        headers: {
+            'Content-Type':'application/x-www-form-urlencoded'
+        },
+        body: JSON.stringify({ data: data }),
+    });
+    return await response.json();
+}
+
+async function getAllKids(idToken) {
+    const message = 'getAllKids';
+    const param = {
+        idToken: idToken,
+    };
+    return post(message, param);
 }
 
 async function getData() {
-    const endPoint = 'https://script.google.com/macros/s/AKfycbyxVh84MzVv0ng2VfSlWfjWi8qc8bI2ROFYSQfBjCJMGXYDFWeT7NU0G0d0BI6GcRNrRg/exec';
     // const idToken = liff.getIDToken();
     const data = {
         message: 'getAllKids',
